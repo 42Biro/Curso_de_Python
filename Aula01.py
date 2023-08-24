d7ce938a07693df514ba175d13b59700ead31e29
Lucas Carvalho <89099213+42Biro@users.noreply.github.com>
2023-08-24 20:38:02 -0300

    Add files via upload

---
+++ Aula01.py
@@ -0,0 +1,58 @@
+# Aula 1: Introdução ao Python
+
+# O Python é uma linguagem de programação de alto nível, interpretada e de propósito geral.
+# Foi criada em 1991 por Guido van Rossum e é uma das linguagens de programação mais populares do mundo.
+
+# O Python é uma linguagem de programação fácil de aprender, pois é simples e concisa.
+# É uma linguagem versátil, que pode ser usada para uma variedade de tarefas, como desenvolvimento de software, data science e machine learning.
+
+# Para começar a programar em Python, você precisará instalar o interpretador Python em seu computador.
+# Você pode encontrar o instalador do Python no site oficial da linguagem.
+
+# Depois de instalar o interpretador Python, você pode começar a escrever código Python.
+# Para escrever código Python, você pode usar um editor de texto ou um IDE (Integrated Development Environment).
+
+# Aqui está um exemplo de código Python:
+
+print("Hello, world!")
+
+# Este código imprime a mensagem "Hello, world!" na tela.
+
+# Para executar o código Python, você pode usar o interpretador Python.
+# Para fazer isso, abra um terminal e digite o seguinte comando:
+
+print("python hello_world.py")
+
+# Este comando irá executar o arquivo `hello_world.py`.
+
+# Aqui estão alguns conceitos básicos do Python:
+
+print('* Variáveis: Uma variável é um espaço na memória do computador que pode ser usado para armazenar dados.')
+print('* Tipos de dados: O Python possui vários tipos de dados, como números, strings e listas.')
+print('* Operadores: Os operadores são usados para realizar operações matemáticas, lógicas e de comparação.')
+print('* Fluxo de controle: O fluxo de controle é usado para controlar a ordem em que o código é executado.')
+print('* Funções: As funções são blocos de código que podem ser reutilizados.')
+
+# Para aprender mais sobre o Python, você pode consultar os seguintes recursos:
+
+print('* [Documentação do Python](https://docs.python.org/3/)')
+print('* [Cursos de Python](https://www.coursera.org/specializations/python)')
+print('* [Livros sobre Python](https://www.amazon.com/Python-Programming-Language-3rd-Edition/dp/0134079786)')
+
+# Tipos de dados
+
+# O Python possui vários tipos de dados, sendo os mais comuns:
+
+print('int: Números inteiros, como 1, 2, 3, 4, 5, etc.')
+print('float: Números reais, como 1.0, 2.0, 3.0, 4.0, 5.0, etc.')
+print('str: Strings, que são sequências de caracteres, como "Hello, world!".')
+print('bool: Valores booleanos, que podem ser verdadeiros (True) ou falsos (False).')
+
+# Para declarar uma variável, você pode usar o seguinte formato:
+
+print('my_int = 1')
+print('my_float = 1.0')
+print('my_str = "Hello, world!"')
+print('my_bool = True')
+
+# Boa sorte em sua jornada de aprendizado do Python!
---
+++ Aula02.py
@@ -0,0 +1,61 @@
+# + -> Adição
+# - -> Subtração
+# * -> Multiplicação
+# / -> Divisão
+# // -> Divisão Inteira
+# % ->
+# int() -> Sempre tentar transformar em int
+
+print()
+
+"""
+ADiÇÃO/SUBTRAÇÃO
+"""
+
+print('ADIÇÃO/SUBTRAÇÃO')
+
+print()
+
+print(20 + 10)
+print(10.5 + 10.5)
+print(10.5 - 5)
+
+print()
+
+"""
+MULTIPLICAÇÃO/DIVISÃO
+"""
+
+print('MULTIPLICAÇÃO/DIVISÃO')
+
+print()
+
+print(20 * 10)
+print(5 / 10.5)
+print(40.5 // 10.5)
+
+print()
+
+"""
+PORCENTAGEM
+"""
+
+print('PORCENTAGEM')
+
+print(20 % 10)
+print(5 % 10.5)
+print(20.5 % 10.5)
+print(9 % 7)
+
+print()
+
+"""
+INT()
+"""
+
+print('int()')
+
+print(type('5'))
+print(type(int('5')))
+int('5')
+print(int('4' + '2'))
