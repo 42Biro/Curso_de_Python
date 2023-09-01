ee92a578f7dbb42dbafd04b73c05aa16a9ffe423
Lucas Carvalho <89099213+42Biro@users.noreply.github.com>
2023-08-31 21:14:11 -0300

    Aula 04

---
+++ Curso_de_Python-main/Aula04-03.py
@@ -0,0 +1,10 @@
+"""
+Faça um algorítmo para ler o salário de um funcionário e aumentá-lo em 15%.
+Após o aumento, desconte 8% de impostos.
+Imprima o salário inicial, o salário com o aumento e o salário final.
+
+"""
+
+print()
+print("😊 Olá sou um algoritmo para calcular quanto a Hortpão vendeu de pães e broas por dia 🍞")
+print()
